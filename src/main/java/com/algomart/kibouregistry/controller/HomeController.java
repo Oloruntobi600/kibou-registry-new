98ac39abdf0e87555d31d49777abd294137bc2e3
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-18 19:07:34 +0100

    new add

--- src/main/java/com/algomart/kibouregistry/controller/HomeController.java
+++ src/main/java/com/algomart/kibouregistry/controller/HomeController.java
@@ -3,8 +3,9 @@ package com.algomart.kibouregistry.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
