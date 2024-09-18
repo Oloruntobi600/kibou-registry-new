ba4bc3532249a574079d19d88ee189a6134901c2
Oloruntobi600 <tobiajayi60@gmail.com>
2024-09-18 18:37:33 +0100

    home controller

--- src/main/java/com/algomart/kibouregistry/controller/HomeController.java
+++ src/main/java/com/algomart/kibouregistry/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomeController {
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<String> HomeController(){
         return new ResponseEntity<>("Welcome to Algomath", HttpStatus.OK);
 
